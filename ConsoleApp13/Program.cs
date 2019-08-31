e9e08dd6a0b014a30070dfc15231a00f3b6b5db7
Aleksandar Puskas <apuskas@gunsh.com>
2019-08-31 09:05:26 +0200

    Import

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Xml.Serialization;
+
+namespace ConsoleApp13
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Write((Derived data) => data.Code);
+            Write((Base data) => data.Code);
+        }
+
+        private static void Write<T1,T2>(Expression<Func<T1,T2>> expression)
+        {
+            var me = (MemberExpression) expression.Body;
+
+            Console.WriteLine(me.Expression.Type.GetProperty(me.Member.Name).GetCustomAttribute<XmlElementAttribute>(false).ElementName);
+        }
+    }
+
+    public abstract class Base
+    {
+        [XmlElement("Base")]
+        public abstract string Code { get; set; }
+    }
+
+    public class Derived : Base
+    {
+        [XmlElement("Derived")]
+        public override string Code { get; set; }
+    }
+}
